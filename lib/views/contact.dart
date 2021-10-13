7008e5f2c804d001ee723baf73f5aecef44d69d1
robinNcode <msmrobin518@gmail.com>
2021-10-13 14:29:39 +0600

    contact page done

--- lib/views/contact.dart
+++ lib/views/contact.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:pencil_box_ui/views/password.dart';
+import 'package:pencil_box_ui/widgets/custom_inputfield.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class ContactPage extends StatelessWidget {
@@ -24,138 +27,97 @@ class ContactPage extends StatelessWidget {
             )),
         body: SafeArea(
           child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: _size.height / 20,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: _size.width / 8),
-                  child: LinearPercentIndicator(
-                    width: _size.width - 100,
-                    lineHeight: 6.0,
-                    percent: 0.4,
-                    progressColor: Colors.green,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: _size.width / 20),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: _size.height / 20,
                   ),
-                ),
-                SizedBox(
-                  height: _size.height / 10,
-                ),
-                const Text(
-                  'Select gender',
-                  style: TextStyle(fontSize: 20),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                      top: _size.height / 20,
-                      left: _size.width / 13,
-                      right: _size.width / 13),
-                  child: Container(
-                    height: 55,
-                    child: Row(
-                      children: const [
-                        FaIcon(FontAwesomeIcons.user,
-                            size: 26, color: Colors.red),
-                        Text(
-                          'Male',
-                          style: TextStyle(fontSize: 20),
-                        )
-                      ],
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey,
-                        style: BorderStyle.solid,
-                        width: 1.0,
-                      ),
-                      color: Colors.transparent,
-                      borderRadius: BorderRadius.circular(8.0),
+                  Padding(
+                    padding: EdgeInsets.only(left: _size.width / 12),
+                    child: LinearPercentIndicator(
+                      width: _size.width - 100,
+                      lineHeight: 6.0,
+                      percent: 0.8,
+                      progressColor: Colors.green,
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                      top: _size.height / 30,
-                      left: _size.width / 13,
-                      right: _size.width / 13),
-                  child: Container(
-                    height: 55,
-                    child: Row(
-                      children: [
-                        Image.asset('assets/images/female-user-icon.png'),
-                        const Text(
-                          'Female',
-                          style: TextStyle(fontSize: 20),
-                        )
-                      ],
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey,
-                        style: BorderStyle.solid,
-                        width: 1.0,
-                      ),
-                      color: Colors.transparent,
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
+                  SizedBox(
+                    height: _size.height / 15,
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                      top: _size.height / 30,
-                      left: _size.width / 13,
-                      right: _size.width / 13),
-                  child: Container(
-                    height: 55,
-                    child: Row(
-                      children: [
-                        FaIcon(FontAwesomeIcons.transgender,
-                            size: 26, color: Colors.blue[900]),
-                        const Text(
-                          'Female',
-                          style: TextStyle(fontSize: 20),
-                        )
-                      ],
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey,
-                        style: BorderStyle.solid,
-                        width: 1.0,
-                      ),
-                      color: Colors.transparent,
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
+                  const Text(
+                    'Provide your Mobile No and Email Address (atleast one)',
+                    style: TextStyle(fontSize: 18,),
                   ),
-                ),
-                SizedBox(
-                  height: _size.height / 10,
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Column(
+                  SizedBox(
+                    height: _size.height / 30,
+                  ),
+                  const CustomField(labelText: 'Country Code', labelColor: Color(0xFF2E64A4), hintText: 'Bangladesh (+088)',),
+                  SizedBox(
+                    height: _size.height / 40,
+                  ),
+                  const CustomField(labelText: 'Mobile Number', labelColor: Color(0xFF2E64A4), hintText: '',),
+                  SizedBox(
+                    height: _size.height / 30,
+                  ),
+                  Column(
                     children: [
-                      Container(
-                        margin: EdgeInsets.only(top: _size.height / 20),
-                        child: const Text(
-                          'For any help',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.black,
-                          ),
+                      Row(children: <Widget>[
+                        Expanded(
+                          child: Container(
+                              margin:
+                              const EdgeInsets.only(left: 8, right: 8),
+                              child: const Divider(
+                                color: Colors.black,
+                                height: 36,
+                              )),
                         ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: _size.height / 60),
-                        child: const Text(
-                          'Call at 16479',
-                          style: TextStyle(fontSize: 20, color: Colors.red),
+                        const Text("OR", style: TextStyle(fontWeight: FontWeight.bold),),
+                        Expanded(
+                          child: Container(
+                              margin: const EdgeInsets.only(left: 8, right: 8),
+                              child: const Divider(
+                                color: Colors.black,
+                                height: 36,
+                              )),
                         ),
-                      ),
+                      ]),
                     ],
                   ),
-                )
-              ],
+                  SizedBox(
+                    height: _size.height / 40,
+                  ),
+                  const CustomField(labelText: 'Email', labelColor: Color(0xFF2E64A4), hintText: '',),
+                  SizedBox(
+                    height: _size.height / 8,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: _size.height / 20),
+                          child: const Text(
+                            'For any help',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: _size.height / 60),
+                          child: const Text(
+                            'Call at 16479',
+                            style: TextStyle(fontSize: 20, color: Colors.red),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
